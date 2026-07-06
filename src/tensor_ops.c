2039b66cd1a928054b630b54bec141495831ede0
nisbenz <benazzaanis783@gmail.com>
2026-07-07 00:15:15 +0100

    feat : implement t_sigmoid

--- src/tensor_ops.c
+++ src/tensor_ops.c
@@ -303,3 +303,25 @@ tensor* t_tanh(tensor* t) {
 
     return out;
 }
+tensor* t_sigmoid(tensor* t) {
+    tensor* contig_t = t;
+    int is_temp_view = 0;
+
+    if (!is_contiguous(t)) {
+        contig_t = t_contiguous(t);
+        is_temp_view = 1;
+    }
+
+    tensor* out = t_alloc(contig_t->ndim, contig_t->dims);
+
+    for (int i = 0; i < out->storage->size; ++i) {
+        float x = contig_t->storage->data[i];
+        out->storage->data[i] = 1/(1+ expf(-x));
+    }
+
+    if (is_temp_view) {
+        t_free(contig_t);
+    }
+
+    return out;
+}
